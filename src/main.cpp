3a4e1f9ef5f16b22c8ac551e90f4783090855b6b
Oliver Holmberg <63006762+Oliver1526@users.noreply.github.com>
2026-04-23 18:19:29 +0200

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,47 +2,8 @@
 #include <iostream>
 #include <vector>
 #include <map>
-#include <memory>
-#include <random>
 
-
-
-
-Camera2D OpenWindow(int gridW, int gridH, int tileSize){
-
-    float mapW = gridW * tileSize;
-    float mapH = gridH * tileSize;
-    
-
-    
-    int monitor = GetCurrentMonitor();
-    // int monitorW = GetMonitorWidth(monitor);
-    // int monitorH = GetMonitorHeight(monitor);
-    int monitorW = 1080*2;
-    int monitorH = 720*2;
-    int winW = monitorW/2;
-    int winH = monitorH/2;
-
-    SetWindowSize(winW, winH); 
-    SetWindowPosition(monitorW/6, monitorH/8);
-
-    Camera2D camera = { 0 };
-    camera.target.x = mapW / 2.0f;
-    camera.target.y = mapH / 2.0f;
-    camera.offset.x = winW/2.0f;
-    camera.offset.y = winH/2.0f-tileSize/2.0f;
-    
-    
-    camera.zoom = 1.0f;
-
-    return camera;
-}
-
-class TextureLoader{
-
-    
-    
-
+class TextureHandler{
     public:
     std::map<std::string, Texture2D> textures = {};
     void Load(std::string ID, std::string Path){
@@ -56,484 +17,82 @@ class TextureLoader{
         Texture2D tex = textures.at(ID);
         return tex;
     }
-
-
 };
 
-void LoadTextures(TextureLoader& textures){
-    textures.Load("SUNFLOWER", "resources/sunflower.png");
-    textures.Load("PEASHOOTER", "resources/peashooter.png");
-    textures.Load("ZOMBIE", "resources/zombie.png");
+TextureHandler textures;
+
+void LoadTextures(){
+    textures.Load("DEBUG", "resources/CatDebug.jpg");
+    textures.Load("GRASS", "resources/Grass.png");
+    textures.Load("DIRT", "resources/CatDebug.jpg");
+    textures.Load("STONE", "resources/CatDebug.jpg");
+    textures.Load("GOLD", "resources/CatDebug.jpg");
+}
+
+void DrawTextureFromTextures(Texture2D texture, Rectangle rec){
+    
+    Rectangle sourceRec = {0.0f, 0.0f, (float)texture.width, (float)texture.height};
+
+
+    DrawTexturePro(texture, sourceRec, rec, {0,0}, 0.0f, WHITE);
 }
 
 
-
-
-
-
-
-class Tile{
-    float x;
-    float y;
-    float RecLength;
-    bool isPlanted = false;
-    
-    public:
-    bool hasPlant() const {return isPlanted;}
-    void setPlanted(bool value) {isPlanted = value;}
+class TextureBlock{
+    int x;
+    int y;
+    int w;
+    int h;
     Rectangle rec;
-    Color color = RED;
-
-    Tile(float posX, float posY, float RecSideLength){
+    std::string ID;
+    public:
+    TextureBlock(int posX, int posY, int W, int H, std::string textureID){
         x = posX;
         y = posY;
-        RecLength = RecSideLength;
-        rec = {x, y, RecLength, RecLength};
-        
+        w = W;
+        h = H;
+        rec = {(float)x, (float)y, (float)w, (float)h};
+        ID = textureID;
     }
 
     void DrawTile(){
-        DrawRectangleRounded(rec, 0.1f, 1.0f, color);
-    }
-};
-
-class Entity{
-    protected:
-    int x;
-    int y;
-    float hp;
-    bool canAttack;
-
-    public: 
-    bool isDead = false;
-    
-    std::string typeID;
-    Rectangle rec;
-    Entity(int posX, int posY, std::string ID, Rectangle rectangle, float health, bool canAttackZombie) : x(posX), y(posY), typeID(ID), rec(rectangle), hp(health), canAttack(canAttackZombie){}
-
-    void DrawEntity(Texture2D texture ){
-        Rectangle sourceRec = {0.0f, 0.0f, (float)texture.width, (float)texture.height};
-        Vector2 origin = {0, 0};
-        
-        DrawTexturePro(texture, sourceRec, rec, origin, 0.0f, WHITE);
-    }
-
-    virtual void TakeDamage(float amount){
-        hp -= amount;
-        if(hp <= 0){
-            isDead = true;
-        }
-    }
-
-    virtual void Attack(float dt, float attackDamage, std::vector<Zombie> zombieVector){
-        if(canAttack){
-            
-        }else{
-
-        }
-    }
-
-    virtual ~Entity() = default;
-};
-
-class Plant : public Entity{
-    int sunCost;
-
-
-    public: 
-    Plant(int posX, int posY, std::string ID, int cost, Rectangle rec, float hp, bool canAttackZombie) : Entity(posX, posY, ID, rec, hp, canAttackZombie){
-        sunCost = cost;
+        DrawTextureFromTextures(textures.Get(ID), rec);
     }
 
 };
 
-
-
-
-class Card{
-    protected:
-    int width;
-    int height;
-    Texture2D texture;
-    Rectangle rec;
-    std::string typeID;
+class Tile : public TextureBlock{
     public:
-    std::string getTypeID() const {return typeID;};
-    int getWidth() const {return width;}
-    int getHeight() const {return height;}
-    Rectangle getRec() const {return rec;}
-
-    Card(int Width, int Height, Texture2D Texture, std::string ID){
-        width = Width;
-        height = Height;
-        texture = Texture;
-        typeID = ID;
-    }
-
-    void DrawCard(int x, int y){
-        rec = {(float)x, (float)y, (float)width, (float)height};
-        Rectangle sourceRec = {0.0f, 0.0f, (float)texture.width, (float)texture.height};
-        Vector2 origin = {0, 0};
-        DrawRectangle(x, y, width, height, BLACK);
-        DrawTexturePro(texture, sourceRec, rec, origin, 0.0f, WHITE);
-    }
-};
-
-
-
-class Inventory{
-    int x;
-    int y;
-    int width;
-    int height;
-    int spacing = 10;
-    std::vector<Card> cards = {};
-    Rectangle rec;
-    public:
-    Rectangle getRec() const {return rec;}
-    std::vector<Card> getCards() const {return cards;}
-    Inventory(int posX, int posY){
-        x = posX;
-        y = posY;
-        
-    }
-
-    void AddCardToInventory(Card card){
-        cards.push_back(card);
-        width = card.getWidth()*cards.size() + (spacing*(cards.size()-1)) + 20;
-        height = card.getHeight() + 20;
-        rec = {(float)x, (float)y, (float)width, (float)height};
-    }
-
-    void DrawInventory(){
-        DrawRectangleRec(rec, WHITE);
-        DrawRectangle(x, y, width, height, GREEN);
-        for (int i = 0; i < cards.size(); i++){
-            cards[i].DrawCard(this->x + 10 + (i*(cards[i].getWidth() + spacing)), this->y+10);
-        }
-    }
-    
-};
-
-class Zombie{
-    int x;
-    int y;
-    int width;
-    int height;
-    Texture2D texture;
-    std::string typeID;
-    Rectangle rec;
-    Rectangle collisionRec;
-    float movementSpeed;
-    float hp;
-
-    int moveIDX = 0;
-    bool isBlocked = false;
-    float attackDamage;
-    float attackTimer = 0.0f;
-    float attackInterval = 1.0f;
-    
-
-    public:
-    Zombie(int posX, int posY, int Width, int Height, Texture2D Texture, std::string type){
-        x = posX;
-        y = posY;
-        width = Width;
-        height = Height;
-        texture = Texture;
-        typeID = type;
-        SetTypeVariables();
-    }
-    
-    void SetTypeVariables(){
-        if(typeID == "Normal"){
-            movementSpeed = 1;
-            hp = 10.0f;
-            attackDamage = 20.0f;
-        }
-    }
-
-    void DrawZombie(){
-        Vector2 pos = {(float)x, (float)y};
-        Rectangle source = {0.0f, 0.0f, (float)texture.width, (float)texture.height};
-        rec = {(float)x, (float)y, (float)width, (float)height};
-        Vector2 origin {0,0};
-        DrawTexturePro(texture, source, rec, origin, 0.0f, WHITE);
-    }
-
-    
-
-    void Move(std::vector<std::unique_ptr<Entity>>& plants){
-        collisionRec = {(float)x - 5, (float)y, (float)width/2, (float)height};
-        isBlocked = false;
-        for (int i = 0; i < plants.size(); i++){
-            if (CheckCollisionRecs(collisionRec, plants[i]->rec)){
-                isBlocked = true;
-            
-                attackTimer += GetFrameTime();
-                if(attackTimer >= 1.0f){
-                    plants[i]->TakeDamage(attackDamage);
-                    attackTimer = 0.0f;
-                }
-            }
-        }
-        if(!isBlocked){
-            moveIDX += 1;
-                if (moveIDX >= 10){
-                    x -= movementSpeed*1;
-                    moveIDX = 0;
-                }
-        }
-    }
-};
-
-class Sunflower : public Plant{
-
-    public:
-    Sunflower(int posX, int posY, int cost, Rectangle rec) : Plant(posX, posY, "SUNFLOWER", cost, rec, 100.0f, false){
-        
-    }
-};
-
-class Peashooter : public Plant{
-
-    public:
-    Peashooter(int posX, int posY, int cost, Rectangle rec) : Plant(posX, posY, "PEASHOOTER", cost, rec, 100.0f, true){
-        
-    }
-    
-};
-
-
-class Player{
-
-    std::string selectedType;
-    TextureLoader& textures;
-    std::vector<std::unique_ptr<Entity>>& entityVector;
-    Camera2D camera;
-    public:
-    Player(std::string Type, TextureLoader& textureloader, std::vector<std::unique_ptr<Entity>>& entityList, Camera2D playerCam)
-        : selectedType(Type), textures(textureloader), entityVector(entityList), camera(playerCam){}
-    
-    void Interaction(std::vector<Tile>& tileVector, Inventory& inventory){
-        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-            Vector2 mousePos = GetScreenToWorld2D(GetMousePosition(), camera);
-            if(CheckCollisionPointRec(mousePos, inventory.getRec())){
-                for (int i = 0; i < inventory.getCards().size(); i++){
-                    if (CheckCollisionPointRec(mousePos, inventory.getCards()[i].getRec())){
-                        selectedType = inventory.getCards()[i].getTypeID();
-                    }
-                }
-            }else{
-                for(int i = 0; i < tileVector.size(); i++){
-                    if(CheckCollisionPointRec(mousePos, tileVector[i].rec)){
-                        tileVector[i].color = BLUE;
-                        float x = tileVector[i].rec.x;
-                        float y = tileVector[i].rec.y;
-                        if(tileVector[i].hasPlant() == false){
-                            PlantPlant(tileVector[i]);
-                            
-                        }
-                        
-                    }
-                }
-            }
-            
-            
-        }
-    }
-
-    void PlantPlant(Tile& tile){
-        if(textures.textures.find(selectedType) != textures.textures.end()){
-            float x = tile.rec.x;
-            float y = tile.rec.y;
-            
-            if(selectedType == "SUNFLOWER"){
-                entityVector.push_back(std::make_unique<Sunflower>(x, y, 100, tile.rec));
-                tile.setPlanted(true);
-            }
-            if(selectedType == "PEASHOOTER"){
-                entityVector.push_back(std::make_unique<Peashooter>(x, y, 100, tile.rec));
-                tile.setPlanted(true);
-            }
-            else{
-
-            }
-        }
-    }
-};
-
-
-
-
-class Level{
-    
-    std::vector<Card> levelCards;
-    std::vector<Zombie> activeZombies;
-    int waves = 5;
-    float spawnTimer = 0.0f;
-    float spawnInterval = 5.0f;
-    Texture2D zombieTex;
-    
-    public:
-    Level(std::vector<std::string> CardIDList, TextureLoader& textures){
-        for (auto item : CardIDList){
-            Card card(100, 100, textures.Get(item), item);
-            levelCards.push_back(card);
-        }
-        zombieTex = textures.Get("ZOMBIE");
-    }
-
-    void Update(float deltaTime, int gridW, int gridH, int tileSize, std::vector<std::unique_ptr<Entity>>& plants){
-        spawnTimer += deltaTime;
-
-        if(spawnTimer >= spawnInterval){
-            int randomRow = GetRandomValue(0, gridH-1);
-
-            int spawnX = gridW * tileSize;
-            int spawnY = randomRow * tileSize;
-
-
-            activeZombies.push_back(Zombie(spawnX, spawnY, tileSize, tileSize, zombieTex, "Normal"));
-
-            spawnTimer = 0.0f;
-        }
-
-        for (int i = 0; i < activeZombies.size(); i++){
-            activeZombies[i].Move(plants);
-        }
-    }
-
-    void DrawZombie(){
-        
-        for (auto& z : activeZombies){
-            z.DrawZombie();
-            
-        }
-    }
-
-    void LoadInventory(Inventory& inventory){
-        for (int i = 0; i < levelCards.size(); i++)
-        inventory.AddCardToInventory(levelCards[i]);
-    }
-
-    
-    
-    
-};
-
-class LevelHandler{
-    std::vector<Level> levels;
-    public:
-
-
-    void AddLevel(Level level){
-        levels.push_back(level);
-    }
-
-    Level LoadLevel(int levelNumber){
-        Level level = levels[levelNumber-1];
-        return level;
-    }
-
-    
-
+    Tile(int posX, int posY, int W, int H, std::string textureID) : TextureBlock(posX, posY, W, H, textureID){}
 
 };
-
 
 
 
 
 int main(){
-    InitWindow(0, 0, "Plants VS Zombies");
+    
+    int winW = 1080;
+    int winH = 720;
+    
     int FPS = 60;
-    SetTargetFPS(FPS);
-    int gridW = 9;
-    int gridH = 5;
-    float targetW = 2620.0f;
-    float scale = (float)GetScreenWidth() / targetW;
-    int tileSize = 100* scale;
-    Camera2D camera = OpenWindow(gridW, gridH, tileSize);
 
-    TextureLoader textures;
-    LoadTextures(textures);
+    InitWindow(winW, winH, "Terraria");
+    
+    LoadTextures();
 
-    Inventory inventory(camera.target.x- camera.offset.x + 10, camera.target.y - camera.offset.y + 10);
+    Tile tile(100, 100, 100, 100, "GRASS");
 
-    // Level Intitializer   
-    Level* level1 = new Level({"SUNFLOWER", "PEASHOOTER"}, textures);
-    std::vector<Zombie> level1Zombies = {Zombie(tileSize*gridW, tileSize*gridH, tileSize, tileSize, textures.Get("ZOMBIE"), "Normal")};
-    level1->LoadInventory(inventory);
-
-    
-    
-    
-
-    
-
-    
-
-    std::vector<Tile> tileVector = {};
-    for (int y = 0; y <= gridH; y++){
-       for (int x = 0; x < gridW; x++){
-        Tile tile(x*tileSize, y*tileSize, tileSize);
-        tileVector.push_back(tile);
-       }
-    }
-
-    
-
-     
-    
-    
-    std::vector<std::unique_ptr<Entity>> entityVector = {};
-    
-
-    Player player("SUNFLOWER", textures, entityVector, camera);
-    
-    
 
     while(!WindowShouldClose()){
 
-
-        float dt = GetFrameTime();
-
-        level1->Update(dt, gridW, gridH, tileSize, entityVector);
-        player.Interaction(tileVector, inventory);
-        
         BeginDrawing();
 
-        ClearBackground(WHITE);
-        
-        BeginMode2D(camera);
+        tile.DrawTile();
 
-        for (int i = 0; i < tileVector.size(); i++){
-            tileVector[i].DrawTile();
-        }
-        for (int i = 0; i < entityVector.size(); i++){
-            if(entityVector[i]->isDead){
-                entityVector.erase(entityVector.begin() + i);
-                i--;
-            }else{
-                std::string ID = entityVector[i]->typeID;
-                Texture2D tex = textures.Get(ID);
-                entityVector[i]->DrawEntity(tex);
-            }
-            
-        }
-        
-        
-        level1->DrawZombie();
-        inventory.DrawInventory();
-
-        EndMode2D();
         EndDrawing();
-    }
 
+    }   
     CloseWindow();
 
-
-
-    return 0;
 }
